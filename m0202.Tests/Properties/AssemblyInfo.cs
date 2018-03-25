4e74f95aff20687b1fae136c6a34310f557efdb5
lizasixsixsix <lizasixsixsix@gmail.com>
2018-03-26 01:51:35 +0300

    tests project added

---
+++ m0202.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("m0202.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MICROSOFT")]
+[assembly: AssemblyProduct("m0202.Tests")]
+[assembly: AssemblyCopyright("Copyright © MICROSOFT 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d6850dee-a7f2-4081-8450-85b3e5c70d16")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ m0202/Exceptions/CustomIntParserException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace m0202.Exceptions
+{
+    [Serializable]
+    public class CustomIntParserException : Exception
+    {
+        public CustomIntParserException()
+        { }
+
+        public CustomIntParserException(string message) : base(message)
+        { }
+
+        public CustomIntParserException(string message,
+                                        Exception inner) : base(message,
+                                                                inner)
+        { }
+
+        protected CustomIntParserException(SerializationInfo info,
+                                           StreamingContext context) : base(info,
+                                                                            context)
+        { }
+    }
+}
