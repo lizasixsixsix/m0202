b7cd9d50e2ab0d69014b5f526dc232b55d808c06
lizasixsixsix <lizasixsixsix@gmail.com>
2018-03-26 09:57:06 +0300

    some arrangements

--- m0202/CustomIntParser.cs
+++ m0202/CustomIntParser.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
+using m0202.Exceptions;
 
 namespace m0202
 {
     public class CustomIntParser
     {
+        public static bool TryParse(string str,
+                                    out int result)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                throw new CustomIntParserException("String cannot be empty.");
+            }
+
+            str = str.Trim();
+
+            Match match = Regex.Match(str, "^-?[0-9a-fA-F]+$");
+
+            if (!match.Success)
+            {
+                throw new CustomIntParserException("This is not a decimal integer.");
+            }
+
+            result = 0;
+
+            return true;
+        }
+
+        public static int Parse(string str)
+        {
+            return 0;
+        }
     }
 }
