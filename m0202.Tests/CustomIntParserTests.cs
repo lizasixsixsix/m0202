b0e8553cf3d10a5719638d4ae5c02ef203c5b180
lizasixsixsix <lizasixsixsix@gmail.com>
2018-03-26 13:06:05 +0300

    second test passed

--- m0202.Tests/CustomIntParserTests.cs
+++ m0202.Tests/CustomIntParserTests.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using m0202;
+using m0202.Exceptions;
 
 namespace m0202.Tests
 {
@@ -10,10 +11,23 @@ namespace m0202.Tests
     {
         [TestMethod]
         [DataRow("77", 77)]
+        [DataRow("-77", -77)]
+        [DataRow("0", 0)]
         public void Parse_CorrectString_ExpectedResult(
             string str, int number)
         {
             Assert.AreEqual(number, CustomIntParser.Parse(str));
+        }
+
+        [TestMethod]
+        [DataRow("77i")]
+        [DataRow("7 7")]
+        [DataRow("7.7")]
+        [ExpectedException(typeof(CustomIntParserException))]
+        public void Parse_IncorrectString_ExceptionThrown(
+            string str)
+        {
+            CustomIntParser.Parse(str);
         }
     }
 }
